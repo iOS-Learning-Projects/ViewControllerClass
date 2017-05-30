52c7ba5a4705c2eaf83b178d1b578e96cdf11002
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2017-05-30 14:25:56 -0300

    Initial Commit

---
+++ ViewControllerClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewControllerClass
+//
+//  Created by Eduardo Vital Alencar Cunha on 30/05/17.
+//  Copyright © 2017 Vital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
